f142c123971adb83d2e57aa15f3e8404c09e179d
pouria-nayeb <51742564+pouria-nayeb@users.noreply.github.com>
2020-03-31 13:53:14 +0430

    add identity tables.

---
+++ kennel-bambino.web/Data/Migrations/20200331091159_AddIdentityDbSets.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace kennel_bambino.web.Data.Migrations
+{
+    public partial class AddIdentityDbSets : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
